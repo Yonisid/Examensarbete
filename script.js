e15bf7b13d7a914ae2fa8c2453d2100ab25de27a
pedramsh <pedramshabani@hotmail.com>
2022-10-18 13:54:01 +0200

    changes

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const startButton = document.getElementById("startButton");
 const scoreArea = document.getElementById("scoreArea");
 const hide = document.querySelector(".hide");
-
-const inputField = document.querySelector('.name')
+const form = document.querySelector(".form-container")
+const newName = document.querySelector(".newName")
     
 
 startButton.addEventListener('click', function handleClick(event) {
     event.preventDefault()
-    let name = document.querySelector('.name').value
-    name.innerHTML = "";
+    form.style.display = "none"
+    newName.innerHTML = document.querySelector(".name").value + " you got " + "points"
 
   hide.style.display = 'block'
 });
