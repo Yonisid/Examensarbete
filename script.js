4d40a897e71797168235659eb45fc6e9b67b64f4
Elvin Salimov <elvin.salimov@awacademy.training>
2022-10-18 16:21:17 +0200

    just a new commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const startButton = document.getElementById("startButton");
+const scoreArea = document.getElementById("scoreArea");
+const hide = document.querySelector(".hide");
+const form = document.querySelector(".form-container")
+const newName = document.querySelector(".newName")
+    
+
+startButton.addEventListener('click', function handleClick(event) {
+    event.preventDefault()
+    form.style.display = "none"
+    newName.innerHTML = document.querySelector(".name").value + " you got " + "points"
+
+  hide.style.display = 'block'
+});
